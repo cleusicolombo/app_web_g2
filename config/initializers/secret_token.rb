e4c8ad985ceef052b7f8201a17587530bad3ad33
Cleusi Colombo <cleusi1@hotmail.com>
2012-11-06 22:39:42 -0200

    Pasta Sebo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sebo::Application.config.secret_token = 'f9673fe7c3adbf37b1a9818676a8897d69d0e98777243d643a6d20f6f2adebbd88a9fcf60c26e41a84488353861b690ececf30ff6ab94bdadb952dfde0f2f145'
