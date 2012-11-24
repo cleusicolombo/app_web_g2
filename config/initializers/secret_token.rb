574612fdde1fd2c871b3f8c6f2beca04456101ba
Tales Viegas <tales.viegas@MacBook-Pro-de-TI-Latam-2.local>
2012-11-24 13:53:52 -0200

    Correcoes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sebo::Application.config.secret_token = 'f9673fe7c3adbf37b1a9818676a8897d69d0e98777243d643a6d20f6f2adebbd88a9fcf60c26e41a84488353861b690ececf30ff6ab94bdadb952dfde0f2f145'
+Sebo::Application.config.secret_token = '41dbf666840280df0ed9e4c70ba2c61b3b54aed3c6daff68501ec17557f6409d08f75863e50c9e01f3421b7fae0d3bba8b05666cc32b79eb60051c5abb2d88aa'
