14cf975552c3c72a6cb20db3f86fbdd3f9e7bb4c
Cleusi Colombo <cleusi1@hotmail.com>
2012-12-08 20:13:11 -0200

    Modelo USER
    E-mail com estrutura validada.
    Senha com mínimo 6 caracteres.
    Validação de login e e-mails únicos.
    Mensagens de erro se validação falhar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
-class User < ActiveRecord::Base
+﻿class User < ActiveRecord::Base
   attr_accessible :name, :email, :login, :password
 
   validates :name, :email, :login, :password, :presence => true
-  validates :email, :login, :uniqueness => {:case_sensitive => false}#true
-  #validates :login, :case_sensitive => true
+  
+  #Valida estrutura do e-mail, com mensagem de erro
+  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]+.[A-Za-z]*\z/i, :message => " inválido!" 
+  
+  #Valida tamanho da senha, com mensagem de erro
+  validates :password, :length => { :minimum => 6, :message => ": A senha deve ter no mínimo 6 caracteres!" }
+  
+  #Valida login e e-mails únicos, com mensagem de erro
+  validates :email, :login, :uniqueness => {:case_sensitive => false, :message => " já existe. Tente outra vez!" }#true
   
     def authenticate password
     #user = find_by_username(username)
