93ae3343650ad0c3b71809db63f28bc402ebbf09
Tales Viegas <tales.viegas@MacBook-Pro-de-TI-Latam-2.local>
2012-12-10 13:43:17 -0200

    Corrigindo as migracoes

--- db/migrate/20121125140046_create_users.rb
+++ db/migrate/20121125140046_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :login
-      t.password :password
+      t.string :password
 
       t.timestamps
     end
--- db/migrate/20121127005334_create_usersbooks.rb
+++ db/migrate/20121127005334_create_usersbooks.rb
@@ -1,9 +1,9 @@
 class CreateUsersbooks < ActiveRecord::Migration
   def change
     create_table :usersbooks do |t|
-      t.references :users_id
-      t.references :books_id
-	  t.float  :value,	:default => 0
+      t.references :users
+      t.references :books
+	t.float  :value,	:default => 0
 
       t.timestamps
 
